8cc5d30acdbd0e988ce8b9fb5eb4b3ead533ef14
vuckovicstrahinja <strahinja.vuckovic@htecgroup.com>
2023-04-04 12:44:28 +0200

    Add cleanup function in effect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
-  }, [(enteredEmail, enteredPassword)]);
+    const idenfifier = setTimeout(() => {
+      console.log("Validation");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 1000);
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(idenfifier);
+    };
+  }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
